02c9dd12750da095bcd4ca682b3c4e2ef8ac11dd
Cosmin <co5myn92@yahoo.com>
2020-05-03 16:17:52 +0300

    1. Create client 2. Connect client to React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh/',
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <p>Pika Pika</p>
+      </main>
+    </ApolloProvider>
+  );
 }
 
 export default App;
